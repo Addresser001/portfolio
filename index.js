5033bebe58b0da3fe65e70f4a8cd4057ebcfcd31
addresser <sokarithankgod2@gmail.com>
2021-03-27 16:17:35 -1100

    Added some animations

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-// let nav_button = document.getElementById('nav-button');
-// let nav_Toggle_icon1 = document.getElementById('nav-icon-1');
-// let nav_Toggle_icon2 = document.getElementById('nav-icon-2');
-// nav_Toggle_icon2.style.display="none"
-// nav_button.onclick = function(){
-//     if(nav_Toggle_icon1.className =="navbar-toggler-icon"){
-//         nav_Toggle_icon1.style.display="none"
-//         nav_Toggle_icon2.style.display="block"
-//         nav_Toggle_icon1.className ="navigation-icon-2"
 
-//     }else {
-//         nav_Toggle_icon2.style.display="none"
-//         nav_Toggle_icon1.style.display="block"
-//         nav_Toggle_icon1.className ="navbar-toggler-icon"
-//     }
-// }
-
-
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -24,6 +6,19 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
+
+
+  if(document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+    document.getElementById("about_section").className ="about-out";
+
+  }
+
+  if(document.body.scrollTop > 1300 || document.documentElement.scrollTop > 1300) {
+    document.getElementById("project_out").className ="projectShow";
+
+  }
+
+  
 }
 
 
@@ -32,29 +27,28 @@ function myFunction() {
 
 
 
-let about_text =document.getElementById('about-text');
-let about_button =document.getElementById('about-button');
+// let about_text =document.getElementById('about-text');
+// let about_button =document.getElementById('about-button');
 
-about_button.onclick = function(){
-    if(about_text.className =="about-close"){
-        about_text.className = 'about-open';
-        about_button.innerHTML = 'View less'
-    }else{
-        about_text.className = 'about-close';
-        about_button.innerHTML = 'View more'
-    }
-}
+// about_button.onclick = function(){
+//     if(about_text.className =="about-close"){
+//         about_text.className = 'about-open';
+//         about_button.innerHTML = 'View less'
+//     }else{
+//         about_text.className = 'about-close';
+//         about_button.innerHTML = 'View more'
+//     }
+// }
 
 
 
-let form = document.getElementById('form');
-let contact_me_button = document.getElementById('contact-me-button');
 
-contact_me_button.onclick = function(){
-    if(form.className =="form-hide"){
-        form.className ="form-show";
-    }else{
-        form.className ="form-hide";
-    }
-}
+// window.onscroll= function(){showItems()}
+
+// function showItems(){
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         document.getElementById("about_section").className ="about-out";
+
+//     }
+// }
 
